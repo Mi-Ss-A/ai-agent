5e849f52928ad3b002af62bbb70fc58672175185
Hyeonbeen Lee <147581930+Been980804@users.noreply.github.com>
2024-11-25 15:18:17 +0900

    Update kafka_utils.py

--- api/kafka_utils.py
+++ api/kafka_utils.py
@@ -2,6 +2,7 @@ from kafka import KafkaProducer
 import json
 from datetime import datetime
 import time
+import os
 
 KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL", "localhost:9092")  # 기본값은 localhost
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "chat-topic")  # 기본값은 chat-topic
